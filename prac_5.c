4f5951d072baebc7634416ad4089132ace1e85cb
jayesh-149 <matejayesh149@gmail.com>
2025-11-07 20:07:02 +0530

    Update and rename prac_7.c to prac_5.c

--- prac_5.c
+++ prac_5.c
@@ -1,4 +1,8 @@
-
+/*
+AIM: Write a program to insert Number of Nodes in the Binary Tree and Traverse in Inorder
+     Preorder and Post Order and  Search an element in Binary Tree(Display NULL if not found,
+     If found Display Found)
+*/
 #include<stdio.h>
 #include<stdlib.h>
 struct node{
@@ -83,3 +87,4 @@ int main(){
    
 
 }
+
